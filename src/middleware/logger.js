afff0c49baf8fe1ff04660cf075e294880e23377
Piotr <piotr.burdzy.klb@gmail.com>
2021-10-14 21:14:14 +0200

    feat: add logger logic

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,9 +1,7 @@
+const logger = require('pino')();
 module.exports = (req, res, next) => {
-  console.log('IMPLEMENT LOGGER MIDDLEWARE')
-
-  // @TODO: log every request in a format: method - route, e.g. "GET /tasks"
-
-  // @TODO: find a logger package in npm repository and implement it here
+  const loggerMessage = `${req.method} ${req.originalUrl}`;
+  logger.info(loggerMessage);
 
   next()
 }
